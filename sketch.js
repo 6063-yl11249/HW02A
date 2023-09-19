7393283622f924bf9709c3113e04a70695def2e3
Yuanqing <yl11249@nyu.edu>
2023-09-19 01:57:54 -0400

    2 beige-colored quadrilaterals
    Lygia Clark's 3rd artwork

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
 function setup() {
+    createCanvas(600, 600)
+    background(0)
 }
 
 function draw() {
+    noStroke()
+    fill(237,215,183)
+    quad(0, 0, 300, 0, 180, 300, 0, 300)
+    quad(420, 300, 600, 300, 600, 600, 300, 600)
 }
